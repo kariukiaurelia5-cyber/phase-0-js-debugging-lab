6de54ccc8d9752b2c0e95b1c7a7305af3d7882a4
Aurelia Kariuki <kariukiaurelia5@gmail.com>
2025-11-20 19:09:50 +0300

    Effectively using the debugger statement

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,22 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
+    
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
-        totalPrice += pricePerItem;
-    }
+    debugger;
 
+    for (let i = 1; i <= quantity; i++) {
+        totalPrice += pricePerItem;
+        debugger;
+    }
+    debugger;
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
     return totalPrice;
 }
-
+console.log('Total (5 @ 10) =>', calculateDiscountedPrice(5, 10));
+console.log('Total (1 @ 10) =>', calculateDiscountedPrice(1, 10));
 
 
 module.exports = calculateDiscountedPrice;
